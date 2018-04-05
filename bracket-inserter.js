59380011414b365ba9f214eb3d10616a723d1938
mwdowns <mwdowns@gmail.com>
2018-04-05 11:47:01 -0400

    adds functionality for renaming the tag file

--- bracket-inserter.js
+++ bracket-inserter.js
@@ -5,20 +5,22 @@ var rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What is the path to the tag file? ', (answer) => {
-	filePath = '/PATH/TO/LOCAL/FILE/' + answer;
-	fs.readFile(filePath, (err, buffer) => {
-		if (err) {
-			console.log('Problem! Danger! Error! ', err.message);
-		} else {
+rl.question('What is the path to the tag file? ', (textFile) => {
+	rl.question('What do you want to add to the tag name? ', (newTagName) => {
+		filePath = '/Users/matthew.downs/Downloads/' + textFile;
+		var textFileArr = textFile.split(/([0-9]{8}.txt)$/);
+		textFileArr[2] = newTagName;
+		var newFileName = textFileArr[0] + textFileArr[2] + textFileArr[1];
+		fs.readFile(filePath, (err, buffer) => {
+			if (err) console.log('Problem! Danger! Error! ', err.message);
 			var begining = /(Insert)/g;
 			var end = /(here)/gi;
 			var text = buffer.toString().replace(begining, '{{Insert').replace(end, 'here}}');
-			fs.writeFile(filePath, text, (err) => {
+			fs.writeFile(newFileName, text, (err) => {
 				if (err) console.log(err);
 				console.log('saved file');
 			});
-		}
+		});
+		rl.close();
 	});
-	rl.close();
 });
